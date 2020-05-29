f2ba467db69c5f72f29c24e144c86a3d94f8c34e
alygv3 <66139318+alygv3@users.noreply.github.com>
2020-05-29 17:18:33 -0400

    Create resume.js

---
+++ js/resume.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav({
+        edge: 'right',
+        inDuration: '600',
+        outDuration: '400'
+    }
+    );
+  });
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
